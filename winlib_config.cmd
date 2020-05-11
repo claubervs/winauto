cf8d52a16537f59521eb5e75cd0d6f26f9073405
Clauber Vianna <claubervs@gmail.com>
2020-05-11 20:30:05 -0300

    Corrections and working versions with Q drive

--- winlib_config.cmd
+++ winlib_config.cmd
@@ -5,18 +5,18 @@ Echo "Setting Windows Library folders as requested..."
 :: Drive letter
 Set "DRIVE=Q"
 :: Folders from library
-Set "3DOBJ=%DRIVE%:\%%USERNAME%%\3D Objects"
-Set "CONT=%DRIVE%:\%%USERNAME%%\Contacts"
-Set "DESK=%DRIVE%:\%%USERNAME%%\Desktop"
-Set "DOCS=%DRIVE%:\%%USERNAME%%\Documents"
-Set "DLOAD=%DRIVE%:\%%USERNAME%%\Downloads"
-Set "FAVS=%DRIVE%:\%%USERNAME%%\Favorites"
-Set "LINKS=%DRIVE%:\%%USERNAME%%\Links"
-Set "MUSIC=%DRIVE%:\%%USERNAME%%\Music"
-Set "PICS=%DRIVE%:\%%USERNAME%%\Pictures"
-Set "GAMES=%DRIVE%:\%%USERNAME%%\Saved Games"
-Set "SRCH=%DRIVE%:\%%USERNAME%%\Searches"
-Set "VIDS=%DRIVE%:\%%USERNAME%%\Videos"
+Set "3DOBJ=%DRIVE%:\3D Objects"
+Set "CONT=%DRIVE%:\Contacts"
+Set "DESK=%DRIVE%:\Desktop"
+Set "DOCS=%DRIVE%:\Documents"
+Set "DLOAD=%DRIVE%:\Downloads"
+Set "FAVS=%DRIVE%:\Favorites"
+Set "LINKS=%DRIVE%:\Links"
+Set "MUSIC=%DRIVE%:\Music"
+Set "PICS=%DRIVE%:\Pictures"
+Set "GAMES=%DRIVE%:\Saved Games"
+Set "SRCH=%DRIVE%:\Searches"
+Set "VIDS=%DRIVE%:\Videos"
 
 Set "USF=HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
 (
@@ -25,13 +25,16 @@ Set "USF=HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Fold
     Reg Add "%USF%" /V "Desktop" /T REG_EXPAND_SZ /D "%DESK%" /F
     Reg Add "%USF%" /V "Personal" /T REG_EXPAND_SZ /D "%DOCS%" /F
     Reg Add "%USF%" /V "{374DE290-123F-4565-9164-39C4925E467B}" /T REG_EXPAND_SZ /D "%DLOAD%" /F
+    Reg Add "%USF%" /V "{7D83EE9B-2244-4E70-B1F5-5393042AF1E4}" /T REG_EXPAND_SZ /D "%DLOAD%" /F
     Reg Add "%USF%" /V "Favorites" /T REG_EXPAND_SZ /D "%FAVS%" /F
     Reg Add "%USF%" /V "{BFB9D5E0-C6A9-404C-B2B2-AE6DB6AF4968}" /T REG_EXPAND_SZ /D "%LINKS%" /F
     Reg Add "%USF%" /V "My Music" /T REG_EXPAND_SZ /D "%MUSIC%" /F
     Reg Add "%USF%" /V "My Pictures" /T REG_EXPAND_SZ /D "%PICS%" /F
+    Reg Add "%USF%" /V "{0DDD015D-B06C-45D5-8C4C-F59713854639}" /T REG_EXPAND_SZ /D "%PICS%" /F
     Reg Add "%USF%" /V "{4C5C32FF-BB9D-43B0-B5B4-2D72E54EAAA4}" /T REG_EXPAND_SZ /D "%GAMES%" /F
     Reg Add "%USF%" /V "{7D1D3A04-DEBB-4115-95CF-2F29DA2920DA}" /T REG_EXPAND_SZ /D "%SRCH%" /F
     Reg Add "%USF%" /V "My Video" /T REG_EXPAND_SZ /D "%VIDS%" /F
 )>NUL
 
-Shutdown /R /D P:2:4
+::Shutdown /R /D P:2:4
+pause
