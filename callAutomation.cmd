43c2aa818195bda6f1fe25379806ca3bc3458cc1
Clauber Vianna <claubervs@gmail.com>
2020-05-28 14:18:56 -0300

    fixed callAutomation

--- callAutomation.cmd
+++ callAutomation.cmd
@@ -1,5 +1,6 @@
-SETLOCAL ENABLEEXTENSIONS
+SETLOCAL
 @echo off
+
 :prepare
 set filesPATH="%WINDIR%\Setup\Files"
 :: Create new folder in homedrive for scripts and files
@@ -11,50 +12,44 @@ copy "%filesPATH%\wget\*.*" "%automationPATH%\"
 copy "%filesPATH%\wget\*.*" "%WINDIR%\System32\"
 :: deletes wget files and dependencies from original folder
 rd /s /q "%filesPATH%\wget"
-:start
-:: enter automation path for script execution
+:: goes inside automation path for script execution
 cd %automationPATH%\
+
+:begin
+:: asks for user input if he/she wants to enter a default download URL or go on with default file
 choice /c:dc /n /t 10 /d d /m "[C]ustom or [D]efault configuration file download? Wait 10 seconds for default option."
 if %ERRORLEVEL% == 1 GOTO download_default
 if %ERRORLEVEL% == 2 GOTO download
+
 :download
-:: Enter download URL or continues with default
+:: Enter download URL or continues with default if left empty
 set /p dURL="Enter download URL or leave blank for default (file must be named automation.cmd):"
 if [%dURL%] == [] GOTO download_default ELSE GOTO download_custom
+
 :download_custom
+:: downloads the file selected by the user
+:: must be named automation.cmd otherwise script will not run after download
 wget.exe --no-check-certificate --content-disposition "%dURL%"
 GOTO after_download
+
 :download_default
+:: downloads the default automation.cmd and goes on with the script
 wget.exe --no-check-certificate --content-disposition "https://raw.githubusercontent.com/claubervs/winauto/master/automation.cmd"
 GOTO after_download
+
 :after_download
-if not exist %automationPATH%\automation.cmd GOTO download_default
+:: verifies if the file is downloaded otherwise goes through the whole process again
+if not exist %automationPATH%\automation.cmd GOTO begin
+:: set exec as the file to execute
 set exec="%automationPATH%\automation.cmd"
 GOTO main
+
 :main
+:: runs the automation downloaded and finishes the script
 call exec
 GOTO finish
-:clean
+::clean
 ::rd /s /q %automationPATH%
-GOTO finish
+::GOTO finish
 :finish
 exit
-
-ENDLOCAL
-
-REM echo.
-REM echo. "Cleaning Windows Apps..."
-REM start /b /wait powershell.exe -file C:\clean_windows_apps.ps1
-REM ::start /b /wait cmd /C "C:\clean_windows_apps.cmd"
-REM echo. "Automating install..."
-REM start /b /wait powershell.exe -file C:\runAutomation.ps1
-REM ::start /b /wait cmd /C "& ""C:\runAutomation.ps1"""
-REM echo.
-REM echo.
-REM echo.
-REM echo "Finished automation. Cleaning up..."
-REM del /f /q C:\clean_windows_apps.ps1
-REM del /f /q C:\runAutomation.ps1
-REM timeout /t 5 /nobreak >nul
-REM del /s /q %0 >nul
-REM exit
