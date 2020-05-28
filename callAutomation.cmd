f0e9ce6e988577a9857e743851376e071cff0f98
Clauber Vianna <claubervs@gmail.com>
2020-05-28 14:05:30 -0300

    fixes

--- callAutomation.cmd
+++ callAutomation.cmd
@@ -1,24 +1,44 @@
 SETLOCAL ENABLEEXTENSIONS
 @echo off
+:prepare
 set filesPATH="%WINDIR%\Setup\Files"
 :: Create new folder in homedrive for scripts and files
 if not exist "C:\automation\" mkdir "C:\automation"
 set automationPATH="C:\automation"
-copy /B "%filesPATH%\wget.exe" "%automationPATH%\"
-copy /B "%filesPATH%\wget.exe" "%WINDIR%\System32\"
-cd %automationPATH%\
-:: Enter download URL or continues with default
+:: copy wget and dependencies to automation folder
+copy "%filesPATH%\wget\*.*" "%automationPATH%\"
+:: copy wget and dependencies to system32 folder
+copy "%filesPATH%\wget\*.*" "%WINDIR%\System32\"
+:: deletes wget files and dependencies from original folder
+rd /s /q "%filesPATH%\wget"
 :start
-set /p dURL="Enter download URL or leave blank for default:"
-if [%dURL%] == [] GOTO default ELSE GOTO custom
-:custom
-wget.exe 
-:default
-wget.exe 
-echo "Enter download URL or leave blank for default:"
-
+:: enter automation path for script execution
+cd %automationPATH%\
+choice /c:dc /n /t 10 /d d /m "[C]ustom or [D]efault configuration file download? Wait 10 seconds for default option."
+if %ERRORLEVEL% == 1 GOTO download_default
+if %ERRORLEVEL% == 2 GOTO download
+:download
+:: Enter download URL or continues with default
+set /p dURL="Enter download URL or leave blank for default (file must be named automation.cmd):"
+if [%dURL%] == [] GOTO download_default ELSE GOTO download_custom
+:download_custom
+wget.exe --no-check-certificate --content-disposition "%dURL%"
+GOTO after_download
+:download_default
+wget.exe --no-check-certificate --content-disposition "https://raw.githubusercontent.com/claubervs/winauto/master/automation.cmd"
+GOTO after_download
+:after_download
+if not exist %automationPATH%\automation.cmd GOTO download_default
+set exec="%automationPATH%\automation.cmd"
+GOTO main
+:main
+call exec
+GOTO finish
+:clean
+::rd /s /q %automationPATH%
+GOTO finish
 :finish
-
+exit
 
 ENDLOCAL
 
