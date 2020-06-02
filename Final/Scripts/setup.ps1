93da4a44284763ac5101f09643d26da453ef98be
Clauber Vianna <claubervs@gmail.com>
2020-06-02 20:32:07 -0300

    setup created

--- Final/Scripts/setup.ps1
+++ Final/Scripts/setup.ps1
@@ -6,15 +6,21 @@ $default_policy = Get-ExecutionPolicy -Scope CurrentUser
 Set-ExecutionPolicy Bypass -Scope CurrentUser
 
 
-$credentials= Get-Credential
+$credentials=Get-Credential
 
 # Get-Credential CVS20181104\claub
 
-Write-Host Installing Chocolatey and Boxtarter
+Write-Host ""
+Write-Host "========================================="
+Write-Host "   Installing Chocolatey and Boxtarter"
+Write-Host "========================================="
+Write-Host ""
 . { Invoke-WebRequest -useb https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force
 
+choco feature enable --name=allowGlobalConfirmation -y
+
 Install-BoxstarterPackage `
-    -PackageName https://gist.githubusercontent.com/claubervs/3a092ec4b6a314580473dcf26bf23b46/raw/config.ps1 -Credential $credentials
+    -PackageName https://gist.githubusercontent.com/claubervs/3a092ec4b6a314580473dcf26bf23b46/raw/config.ps1 -Credential $credentials -DisableReboots
 
 # change execution policy back to default
 Set-ExecutionPolicy $default_policy -Scope CurrentUser
